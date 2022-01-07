9ebc47940588382a010defb130541380e456bcd5
Joy Cheng <joy.cheng97324@gmail.com>
2022-01-07 18:07:22 +0800

    week1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
+import router from './router'
 
 const vue = createApp(App)
 vue.use(VueAxios, axios)
+vue.use(router)
 vue.mount('#app')
